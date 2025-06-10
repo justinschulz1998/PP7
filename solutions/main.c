d0e2a108c971e7a29cf71bb205e0176a72de278b
justin schulz <justinschulz2014hotmail.de>
2025-06-10 14:21:52 +0200

    Aufgabe 3

---
+++ solutions/main.c
@@ -0,0 +1,9 @@
+// main.c
+ #include <stdio.h>
+ extern int add(int, int);
+
+ int main(void) {
+     int sum = add(5, 7);
+         printf("5 + 7 = %d\n", sum);
+             return 0;
+             }
